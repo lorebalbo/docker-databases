5d4296ca1bbb38725e852de76adb265ee5ddc61f
lorebalbo <lorenzo02.balboni@edu.unife.it>
2025-04-19 17:02:09 +0000

    compose splitted for a better integration in other projects
    The main compose has been splitted in multiple compose, one for each service. This repo can now integrates with another projects. Also the warnings are removed.
    A network is created when running the up.sh script so that all the service can comunicate with each other. The network can be deleted when executing the down.sh script with the -n or --networks option.

--- bin/down.sh
+++ bin/down.sh
@@ -2,18 +2,22 @@
 
 set -e
 
+source .env
+
 # Display help message if invalid arguments are passed
 show_help() {
     echo "Usage: down.sh [OPTIONS]"
     echo "Options:"
     echo "  -v, --volumes   Remove named volumes declared in the 'volumes' section of the Compose file"
     echo "  --rmi           Remove images used by services"
+    echo "  -n, --networks  Remove the network specified in the DOCKER_NETWORK_NAME environment variable"
     exit 1
 }
 
 # Initialize variables for options
 volumes_flag=""
 rmi_flag=""
+network_flag=""
 
 # Parse arguments
 for arg in "$@"; do
@@ -24,12 +28,24 @@ for arg in "$@"; do
         --rmi)
             rmi_flag="--rmi"
             ;;
+        -n|--networks)
+            network_flag="true"
+            ;;
         *)
             show_help
             ;;
     esac
+    shift
 done
 
 # Run docker compose down with the appropriate flags
 docker compose down $volumes_flag $rmi_flag
-# echo "docker compose down $volumes_flag $rmi_flag"
+
+# Remove the network if -n or --networks is passed
+if [ "$network_flag" == "true" ]; then
+    if [ -z "$DOCKER_NETWORK_NAME" ]; then
+        echo "Error: DOCKER_NETWORK_NAME is not set in the environment."
+        exit 1
+    fi
+    docker network rm "$DOCKER_NETWORK_NAME" || echo "Network $DOCKER_NETWORK_NAME does not exist."
+fi
--- bin/up.sh
+++ bin/up.sh
@@ -4,6 +4,11 @@ set -e
 
 source .env
 
+# Create the Docker network if it doesn't exist
+if ! docker network ls --filter name=^$DOCKER_NETWORK_NAME$ --format "{{.Name}}" | grep -w $DOCKER_NETWORK_NAME > /dev/null; then
+    docker network create ${DOCKER_NETWORK_NAME:-devnet}
+fi
+
 # Function to check if a service is enabled
 is_service_enabled() {
     local service_enabled_var="${1}_ENABLED"
@@ -22,18 +27,25 @@ if [[ "$1" == "-e" || "$1" == "--env-file" ]]; then
         echo "Error: --env-file requires a file path argument."
         exit 1
     fi
+else
+    compose_command+=" --env-file .env"
 fi
-
-compose_command+=" up -d"
 
 # Find all *_ENABLED variables and conditionally add services to the command
 for var in $(compgen -A variable | grep '_ENABLED$'); do
     service="${var%_ENABLED}"
     if is_service_enabled "$service"; then
-        compose_command+=" ${service,,}"
+        compose_command+=" -f composes/docker-compose-${service,,}.yml"
     fi
 done
 
+compose_command+=" up -d"
+
+# if the DOCKER_PROJECT_NAME variable is set, add -p $DOCKER_PROJECT_NAME to the command
+if [[ -n "$DOCKER_PROJECT_NAME" ]]; then
+    compose_command+=" -p $DOCKER_PROJECT_NAME"
+fi
+
 # Run the docker compose command
-# echo "Running docker compose command: $compose_command"
+echo "Running docker compose command: $compose_command"
 eval "$compose_command"
