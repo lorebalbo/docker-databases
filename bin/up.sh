883a0115dab5509e9779cfa8c39c884c877e1d3a
lorebalbo <lorenzo02.balboni@edu.unife.it>
2025-04-20 16:25:20 +0000

    update up

--- bin/up.sh
+++ bin/up.sh
@@ -18,21 +18,56 @@ display_help() {
 
 # Parse arguments and check if env-file is specified
 custom_env_file=""
+
+
+# Function to check if a service is enabled
+is_service_enabled() {
+    local service_enabled_var="${1}_ENABLED"
+    local service_enabled_value="${!service_enabled_var}"
+    [[ "$service_enabled_value" != "false" ]]
+}
+
+compose_command="docker compose"
+
+build=0
+
+# Reset positional parameters for proper argument parsing
+# set -- "${ORIGINAL_ARGS[@]}"
+
+# Parse arguments
 while [[ $# -gt 0 ]]; do
     case $1 in
+        -h|--help)
+            display_help
+            exit 0
+            ;;
+        --build)
+            build=1
+            ;;
+        -f|--file)
+            if [[ -n "$2" ]]; then
+                compose_command+=" -f $2"
+                shift
+            else
+                echo "Error: --file requires a file path argument."
+                exit 1
+            fi
+            ;;
         -e|--env-file)
             if [[ -n "$2" ]]; then
                 custom_env_file="$2"
-                break
+                shift
             fi
+            shift
+            ;;
+        *)
+            echo "Error: Invalid argument $1"
+            echo "Use -h or --help to see available options."
+            exit 1
             ;;
     esac
     shift
 done
-
-# Reset positional parameters
-ORIGINAL_ARGS=("$@")
-set -- "${ORIGINAL_ARGS[@]}"
 
 # Set up the environment file - use custom if provided, otherwise default
 if [[ -n "$custom_env_file" ]]; then
@@ -45,61 +80,7 @@ else
     echo "Warning: No .env file found in $PROJECT_DIR"
 fi
 
-# Create the Docker network if it doesn't exist
-if ! docker network ls --filter name=^$DOCKER_NETWORK_NAME$ --format "{{.Name}}" | grep -w $DOCKER_NETWORK_NAME > /dev/null; then
-    docker network create ${DOCKER_NETWORK_NAME:-devnet}
-fi
-
-# Function to check if a service is enabled
-is_service_enabled() {
-    local service_enabled_var="${1}_ENABLED"
-    local service_enabled_value="${!service_enabled_var}"
-    [[ "$service_enabled_value" != "false" ]]
-}
-
-compose_command="docker compose"
-
-# Add env-file to compose command by default if we found an .env file
-if [[ -n "$ENV_FILE" ]]; then
-    compose_command+=" --env-file $ENV_FILE"
-fi
-
-# Reset positional parameters for proper argument parsing
-set -- "${ORIGINAL_ARGS[@]}"
-
-# Parse arguments
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        -h|--help)
-            display_help
-            exit 0
-            ;;
-        -e|--env-file)
-            if [[ -n "$2" ]]; then
-                # Already handled above, just skip
-                shift
-            else
-                echo "Error: --env-file requires a file path argument."
-                exit 1
-            fi
-            ;;
-        -f|--file)
-            if [[ -n "$2" ]]; then
-                compose_command+=" -f $2"
-                shift
-            else
-                echo "Error: --file requires a file path argument."
-                exit 1
-            fi
-            ;;
-        *)
-            echo "Error: Invalid argument $1"
-            echo "Use -h or --help to see available options."
-            exit 1
-            ;;
-    esac
-    shift
-done
+# echo "$compose_command"
 
 # Find all *_ENABLED variables and conditionally add services to the command
 for var in $(compgen -A variable | grep '_ENABLED$'); do
@@ -108,6 +89,13 @@ for var in $(compgen -A variable | grep '_ENABLED$'); do
         compose_command+=" -f $PROJECT_DIR/composes/docker-compose-${service,,}.yml"
     fi
 done
+
+# Add env-file to compose command by default if we found an .env file
+if [[ -n "$ENV_FILE" ]]; then
+    compose_command+=" --env-file $ENV_FILE"
+fi
+
+# echo "$compose_command"
 
 # if the DOCKER_PROJECT_NAME variable is set, add -p $DOCKER_PROJECT_NAME to the command
 if [[ -n "$DOCKER_PROJECT_NAME" ]]; then
@@ -119,6 +107,15 @@ else
 fi
 
 compose_command+=" up -d"
+
+if [[ $build -eq 1 ]]; then
+    compose_command+=" --build"
+fi
+
+# Create the Docker network if it doesn't exist
+if ! docker network ls --filter name=^$DOCKER_NETWORK_NAME$ --format "{{.Name}}" | grep -w $DOCKER_NETWORK_NAME > /dev/null; then
+    docker network create ${DOCKER_NETWORK_NAME:-devnet}
+fi
 
 # Run the docker compose command
 # echo "Running docker compose command: $compose_command"
